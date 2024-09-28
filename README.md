eb6bf9b28f5ff7fa4e40afd30af72964a7dd5607
Rishi <rishi.helloworld@gmail.com>
2024-09-28 12:16:59 +0530

    Bot completed

---
+++ README.md
@@ -0,0 +1,20 @@
+# Link Remover Telegram Bot
+
+A Python-based **Telegram Bot** for deleting all kind of URLs and links in almost everywhere the bot joined. (Scan new and updated messages)
+
+## Features
+
+- Detect most kind of URL, links without HTTP, HTTPS and with that
+- Supporting new and edited messages
+- Handling hidden admin
+- Handling users who send messages as a Group or channel
+
+## Using
+
+```bash
+$ pip install -r requirements.txt
+or
+$ pip install pyTelegramBotAPI
+```
+
+© Copyright Max Base, 2022
---
+++ bot.py
@@ -0,0 +1,52 @@
+from telebot import TeleBot
+
+# config
+TOKEN = '6747414933:AAH36_yZvjXeBUi59CB8G2VR2sbXcCi1gbc'
+ADMINS = [
+    '771530252'
+]
+
+# setup
+app = TeleBot(TOKEN)
+
+def check_message(type, message):
+    print("\n\n\n\n==============\n\n\n")
+    print(type, message)
+
+    if message.from_user.id in ADMINS:
+        return
+
+    # check getChatMember, check is creator or administrator
+    chat_member = app.get_chat_member(message.chat.id, message.from_user.id)
+    print("==================> User data:", chat_member)
+    if chat_member.status == 'creator' or chat_member.status == 'administrator':
+        return
+    if chat_member.status == 'left' and chat_member.user.username == 'GroupAnonymousBot':
+        return
+
+    if message.text.find('@') != -1:
+        print("Found username in message text")
+        app.delete_message(message.chat.id, message.message_id)
+    elif message.text.find('t.me') != -1:
+        print("Found link to username in message text")
+        app.delete_message(message.chat.id, message.message_id)
+    elif message.text.find('http://') != -1 or message.text.find('https://') != -1:
+        print("Found link to website in message text")
+        app.delete_message(message.chat.id, message.message_id)
+    elif message.text.find('.com') != -1 or message.text.find('.ir') != -1:
+        print("Found link to website in message text")
+        app.delete_message(message.chat.id, message.message_id)
+
+# edit message listener
+@app.edited_message_handler(func=lambda message: True)
+def edit_message(message):
+    check_message("edit", message)
+
+# new message listener
+@app.message_handler(func=lambda message: True)
+def new_message(message):
+    check_message("new", message)
+
+# keep alive
+if __name__ == '__main__':
+    app.infinity_polling()
